85dc7b75dbd70d689d450ad5d0a8c2c056f87b3b
evandroanholeto <evandroanholeto@gmail.com>
2023-01-30 12:30:31 -0300

    Commit
    Commit.

--- 13_express_com_ts/src/app.ts
+++ 13_express_com_ts/src/app.ts
@@ -2,9 +2,17 @@
 // console.log("Express com TypeScript!!!")
 
 // 2 - init express
-import express, { Request, Response } from "express";
+import express, { Request, Response, NextFunction } from "express";
 
 const app = express();
+
+// 11 - middleware para todas as rotas
+function showPath(req: Request, res: Response, next: NextFunction) {
+    console.log(req.path);
+    next();
+}
+
+app.use(showPath);
 
 // 3 - rota com POST
 app.use(express.json());
@@ -75,6 +83,50 @@ app.get("/api/product/:id/review/:reviewId", (req: Request, res: Response) => {
     const reviewId = req.params.reviewId;
 
     return res.send(`Acessando a review ${reviewId} do produto ${productId}`);
+});
+
+// 9 - router handler
+app.get("/api/user/:id", getUser);
+
+function getUser(req: Request, res: Response) {
+    console.log(`Resgatando o usuário com o id: ${req.params.id}`);
+
+    return res.send("O usuário foi encontrado!");
+}
+
+// 10 - middleware
+function checkUser(req: Request, res: Response, next: NextFunction) {
+    if (req.params.id === "1") {
+        console.log("Pode seguir!");
+        next();
+    } else {
+        console.log("Acesso restrito!");
+    }
+}
+
+app.get("/api/user/:id/access", checkUser, (req: Request, res: Response) => {
+    return res.json({
+        msg: "Bem-vindo a área administrativa!"
+    });
+});
+
+// 12 - req e res com generics
+app.get("/api/user/:id/details/:name", (req: Request<{ id: string; name: string }>, res: Response<{ status: boolean }>) => {
+    console.log(`Id: ${req.params.id}`);
+    console.log(`Name: ${req.params.name}`);
+
+    return res.json({ status: true });
+});
+
+// 13 - tratando erros
+app.get("/api/error", (req: Request, res: Response) => {
+    try {
+        throw new Error("Algo deu errado!");
+    } catch (e: any) {
+        res.status(500).json({
+            msg: e.message
+        });
+    }
 });
 
 app.listen(3000, () => {
