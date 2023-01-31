9df10f62d658ad094349cde1b6f88b36c7463463
evandroanholeto <evandroanholeto@gmail.com>
2023-01-31 16:51:34 -0300

    Commit
    Commit.

--- 14_api_restfull_com_ts/src/router.ts
+++ 14_api_restfull_com_ts/src/router.ts
@@ -1,10 +1,14 @@
 import { Router, Request, Response } from "express";
 import { createMovie } from "./controllers/movieController";
 
+// validations
+import { validate } from "./middleware/handleValidation"
+import { movieCreateValidation } from "./middleware/movieValidation";
+
 const router = Router();
 
 export default router
     .get("/test", (req: Request, res: Response) => {
         res.status(200).send("API Working!");
     })
-    .post("/movie", createMovie);
+    .post("/movie", movieCreateValidation(), validate, createMovie);
