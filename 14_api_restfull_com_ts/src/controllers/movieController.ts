4d622f1815a0a283c2599919c7c43ac8a1ada473
evandroanholeto <evandroanholeto@gmail.com>
2023-01-31 17:51:38 -0300

    Commit
    Commit.

--- 14_api_restfull_com_ts/src/controllers/movieController.ts
+++ 14_api_restfull_com_ts/src/controllers/movieController.ts
@@ -11,3 +11,28 @@ export async function createMovie(req: Request, res: Response) {
         Logger.error(`Erro no sistema: ${e.message}`)
     }
 };
+
+export async function findMovieById(req: Request, res: Response) {
+    try {
+        const id = req.params.id
+        const movie = await MovieModel.findById(id)
+
+        if (!movie) {
+            return res.status(404).json({ error: "O filme não existe." })
+        }
+
+        return res.status(200).json(movie)
+    } catch (e: any) {
+        Logger.error(`Erro no sistema: ${e.message}`)
+    }
+};
+
+export async function getAllMovies(req: Request, res: Response) {
+    try {
+        const movies = await MovieModel.find()
+
+        return res.status(200).json(movies)
+    } catch (e: any) {
+        Logger.error(`Erro no sistema: ${e.message}`)
+    }
+}
